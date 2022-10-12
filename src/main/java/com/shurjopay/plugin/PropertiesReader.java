056040b2869c7a2cb7318929d3e0ea8e350dd91b
Al-Amin <alamin.binmannan@shurjomukhi.com.bd>
2022-10-12 16:06:55 +0600

    remove runtimeException from propertiesReader

--- src/main/java/com/shurjopay/plugin/PropertiesReader.java
+++ src/main/java/com/shurjopay/plugin/PropertiesReader.java
@@ -1,12 +1,16 @@
 package com.shurjopay.plugin;
 
+import java.io.IOException;
 import java.util.Properties;
+
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * 
  * @author Al - Amin
  * @since 2022-06-13
  */
+@Slf4j
 public class PropertiesReader {
 	
 	private static PropertiesReader instance = null;
@@ -19,8 +23,9 @@ public class PropertiesReader {
 			Properties prop = new Properties();
 			prop.load(PropertiesReader.class.getClassLoader().getResourceAsStream("shurjopay.properties"));
 			return prop;
-		} catch (Exception e) {
-			throw new RuntimeException("shurjopay.properties is missing in resource path or Resources path is not exist");
+		} catch (IOException e) {
+			log.error("shurjopay.properties is missing in resource path or resources path is not exist", e);
+			return null;
 		}
 	}
 
