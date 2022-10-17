6da81a503ee37fdecc7d8231c2c3b11f7bafb837
Al-Amin <alamin.binmannan@shurjomukhi.com.bd>
2022-10-17 16:50:42 +0600

    finalize shurjoPay config and properties

--- src/main/java/com/shurjopay/plugin/model/ShurjopayConfig.java
+++ src/main/java/com/shurjopay/plugin/model/ShurjopayConfig.java
@@ -5,12 +5,12 @@ import lombok.experimental.Accessors;
 
 /**
  * This model represents the merchant's credentials and shurjoPay URLs
+ * 
  * @author Al-Amin
  * @since 2022-10-12
  */
-@Data
-@Accessors(chain = true)
-public class ShurjoPayConfig {
+@Data @Accessors(chain = true)
+public class ShurjopayConfig {
 
 	private String username;
 	private String password;
